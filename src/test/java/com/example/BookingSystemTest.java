f5c9e226e0e9b6897e5146eecc3721d7114e5e55
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-01-23 17:16:40 +0100

    Add initial skeleton for BookingSystem tests with placeholders for future test cases. Todos for tests.

---
+++ src/test/java/com/example/BookingSystemTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//todo: Skapa tester för BookingSystem.java - både tester för lyckade bokningar samt misslyckade
+//todo: tester för att kunna boka ett rum (bookRoom) med id, starttid, sluttid - giltiga datum
+//todo: tester för att få tillgängliga rum (getAvailableRooms)
+//todo: avboka en bokning (cancelBooking)
+//todo: dokumentera testfallen med javadocs
+class BookingSystemTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+
+}
